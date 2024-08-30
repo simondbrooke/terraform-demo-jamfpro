bfe78ba0d86eacc46395f3c8a50587df25ed4f4f
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-08-30 17:10:11 +0100

    chore: Update Terraform script for Jamf Pro packages

--- workload/terraform/jamfpro/packages.tf
+++ workload/terraform/jamfpro/packages.tf
@@ -28,29 +28,29 @@
 # // https://download.mozilla.org/?product=firefox-latest&os=osx&lang=en-US - firefox
 
 # // Example of referencing a package from a https source (with redirects)
-# resource "jamfpro_package" "jamfpro_package_02" {
-#   package_name          = "tf-ghatest-package-httpsourceprovider-test"
-#   package_file_source   = "https://download.mozilla.org/?product=firefox-latest&os=osx&lang=en-US"
-#   category_id           = "-1"
-#   info                  = "tf package deployment for demonstration"
-#   notes                 = "Uploaded by: terraform-provider-jamfpro plugin."
-#   priority              = 10
-#   reboot_required       = true
-#   fill_user_template    = false
-#   fill_existing_users   = false
-#   os_requirements       = "macOS 10.15.0"
-#   swu                   = false
-#   self_heal_notify      = false
-#   os_install            = false
-#   serial_number         = ""
-#   suppress_updates      = false
-#   ignore_conflicts      = false
-#   suppress_from_dock    = false
-#   suppress_eula         = false
-#   suppress_registration = false
-#   manifest              = ""
-#   manifest_file_name    = ""
-# }
+resource "jamfpro_package" "jamfpro_package_02" {
+  package_name          = "tf-ghatest-package-httpsourceprovider-test"
+  package_file_source   = "https://download.mozilla.org/?product=firefox-latest&os=osx&lang=en-US"
+  category_id           = "-1"
+  info                  = "tf package deployment for demonstration"
+  notes                 = "Uploaded by: terraform-provider-jamfpro plugin."
+  priority              = 10
+  reboot_required       = true
+  fill_user_template    = false
+  fill_existing_users   = false
+  os_requirements       = "macOS 10.15.0"
+  swu                   = false
+  self_heal_notify      = false
+  os_install            = false
+  serial_number         = ""
+  suppress_updates      = false
+  ignore_conflicts      = false
+  suppress_from_dock    = false
+  suppress_eula         = false
+  suppress_registration = false
+  manifest              = ""
+  manifest_file_name    = ""
+}
 
 # // Example of referencing a package from a https source
 # resource "jamfpro_package" "jamfpro_package_03" {
