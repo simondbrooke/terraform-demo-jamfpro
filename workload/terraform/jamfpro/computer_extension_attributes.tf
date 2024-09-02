d210cbbf0d220247cafe05ee6d87ca2784fb009d
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-09-02 06:34:57 +0100

    Refactor Terraform workflow to update computer extension attributes in Jamf Pro

--- workload/terraform/jamfpro/computer_extension_attributes.tf
+++ workload/terraform/jamfpro/computer_extension_attributes.tf
@@ -17,25 +17,25 @@ resource "jamfpro_computer_extension_attribute" "computer_extension_attribute_00
   enabled     = true
   description = "An attribute collected from a text field."
 
-  input_type  = "Text Field"
+  input_type        = "Text Field"
   inventory_display = "Hardware"
 }
 
 resource "jamfpro_computer_extension_attribute" "computer_extension_attribute_003" {
-  name        = "tf-example-cexa-hello-world"
-  enabled     = true
-  description = "An attribute collected via a script."
-  input_type = "script"
-  input_script   = "#!/bin/bash\necho 'Hello, World!!!!! :)'"
+  name         = "tf-example-cexa-hello-world"
+  enabled      = true
+  description  = "An attribute collected via a script."
+  input_type   = "script"
+  input_script = "#!/bin/bash\necho 'Hello, World!!!!! :)'"
 
   inventory_display = "General"
 }
 
 resource "jamfpro_computer_extension_attribute" "computer_extension_attribute_004" {
-  name        = "tf-example-cexa-system_extensions"
-  enabled     = true
-  description = "An attribute collected via a script."
-  input_type  = "script"
+  name         = "tf-example-cexa-system_extensions"
+  enabled      = true
+  description  = "An attribute collected via a script."
+  input_type   = "script"
   input_script = file("support_files/computer_extension_attributes/system_extensions.sh")
 
   inventory_display = "Operating System"
