b3fe3a2166d70c7e669a8142dc657fd3f5a91f3f
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-09-02 07:13:39 +0100

    Refactor Terraform workflow to add new categories in Jamf Pro

--- workload/terraform/jamfpro/categories.tf
+++ workload/terraform/jamfpro/categories.tf
@@ -23,32 +23,32 @@ resource "jamfpro_category" "jamfpro_category_005" {
   priority = 10
 }
 
-# resource "jamfpro_category" "jamfpro_category_006" {
-#   name     = "tf-ghatest-user-experience"
-#   priority = 10
-# }
+resource "jamfpro_category" "jamfpro_category_006" {
+  name     = "tf-ghatest-user-experience"
+  priority = 10
+}
 
-# resource "jamfpro_category" "jamfpro_category_007" {
-#   name     = "tf-ghatest-user-management"
-#   priority = 10
-# }
+resource "jamfpro_category" "jamfpro_category_007" {
+  name     = "tf-ghatest-user-management"
+  priority = 10
+}
 
-# resource "jamfpro_category" "jamfpro_category_008" {
-#   name     = "tf-ghatest-device-management"
-#   priority = 10
-# }
+resource "jamfpro_category" "jamfpro_category_008" {
+  name     = "tf-ghatest-device-management"
+  priority = 10
+}
 
-# resource "jamfpro_category" "jamfpro_category_009" {
-#   name     = "tf-ghatest-peripherals"
-#   priority = 10
-# }
+resource "jamfpro_category" "jamfpro_category_009" {
+  name     = "tf-ghatest-peripherals"
+  priority = 10
+}
 
-# resource "jamfpro_category" "jamfpro_category_010" {
-#   name     = "tf-ghatest-compliance"
-#   priority = 10
-# }
+resource "jamfpro_category" "jamfpro_category_010" {
+  name     = "tf-ghatest-compliance"
+  priority = 10
+}
 
-# resource "jamfpro_category" "jamfpro_category_011" {
-#   name     = "tf-ghatest-maintenance"
-#   priority = 10
-# }
+resource "jamfpro_category" "jamfpro_category_011" {
+  name     = "tf-ghatest-maintenance"
+  priority = 10
+}
