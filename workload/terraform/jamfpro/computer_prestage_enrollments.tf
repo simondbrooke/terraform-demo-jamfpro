7fdb39f87b2213d2cc471fde66c0e8f05d42f950
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-09-10 18:24:59 +0100

    refactor: Update prestage_installed_profile_ids and custom_package_ids in computer_prestage_enrollments.tf

--- workload/terraform/jamfpro/computer_prestage_enrollments.tf
+++ workload/terraform/jamfpro/computer_prestage_enrollments.tf
@@ -168,8 +168,8 @@ resource "jamfpro_computer_prestage_enrollment" "jamfpro_computer_prestage_enrol
   auto_advance_setup                      = false
   install_profiles_during_setup           = true
   prestage_installed_profile_ids          = reverse(sort([jamfpro_macos_configuration_profile_plist.jamfpro_macos_configuration_profile_001.id, jamfpro_macos_configuration_profile_plist.jamfpro_macos_configuration_profile_002.id])) // requires decending order
-  custom_package_ids                      = sort([1, 2])                // requires ascending order
-  custom_package_distribution_point_id    = "-2"                        // "-1" - not used / "-2" - Cloud Distribution Point (Jamf Cloud) / "any other number" - Distribution Point ID
+  custom_package_ids                      = sort([1, 2])                                                                                                                                                                                // requires ascending order
+  custom_package_distribution_point_id    = "-2"                                                                                                                                                                                        // "-1" - not used / "-2" - Cloud Distribution Point (Jamf Cloud) / "any other number" - Distribution Point ID
   enable_recovery_lock                    = true
   recovery_lock_password_type             = "MANUAL" // "MANUAL" / "RANDOM"
   recovery_lock_password                  = "thing"
