e80a80f9d70a1b2d72de30b32114a225022fab3e
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-09-11 09:52:58 +0100

    refactor: Update computer extension attributes, dock items, macOS configuration profiles, scripts, and sites

--- workload/terraform/jamfpro/sites.tf
+++ workload/terraform/jamfpro/sites.tf
@@ -1,12 +1,12 @@
-resource "jamfpro_site" "jamfpro_site_001" {
-  name = "tf-demo-site-uk"
-}
+# resource "jamfpro_site" "jamfpro_site_001" {
+#   name = "tf-demo-site-uk"
+# }
 
-resource "jamfpro_site" "jamfpro_site_002" {
-  name = "tf-demo-site-india"
-}
+# resource "jamfpro_site" "jamfpro_site_002" {
+#   name = "tf-demo-site-india"
+# }
 
-resource "jamfpro_site" "jamfpro_site_003" {
-  name = "tf-demo-site-usa"
-}
+# resource "jamfpro_site" "jamfpro_site_003" {
+#   name = "tf-demo-site-usa"
+# }
 
