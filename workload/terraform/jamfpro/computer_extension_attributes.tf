b3b5fb7c69b10a19b3cb50933fd96b1daaceb9ef
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-09-11 06:27:25 +0100

    refactor: Update computer extension attributes and computer prestage enrollments

--- workload/terraform/jamfpro/computer_extension_attributes.tf
+++ workload/terraform/jamfpro/computer_extension_attributes.tf
@@ -1,42 +1,42 @@
-# Pop-up Menu Example
-resource "jamfpro_computer_extension_attribute" "jamfpro_computer_extension_attribute_popup_menu_1" {
-  name                   = "tf-demo-popup-menu-example"
-  enabled                = true
-  description            = "An attribute collected from a pop-up menu."
-  input_type             = "POPUP"
-  popup_menu_choices     = ["Option 1", "Option 2", "Option 3"]
-  inventory_display_type = "USER_AND_LOCATION"
-  data_type              = "STRING"
-}
+# # Pop-up Menu Example
+# resource "jamfpro_computer_extension_attribute" "jamfpro_computer_extension_attribute_popup_menu_1" {
+#   name                   = "tf-demo-popup-menu-example"
+#   enabled                = true
+#   description            = "An attribute collected from a pop-up menu."
+#   input_type             = "POPUP"
+#   popup_menu_choices     = ["Option 1", "Option 2", "Option 3"]
+#   inventory_display_type = "USER_AND_LOCATION"
+#   data_type              = "STRING"
+# }
 
-# Text Field Example
-resource "jamfpro_computer_extension_attribute" "computer_extension_attribute_text_field_1" {
-  name                   = "tf-demo-text-field-example"
-  enabled                = true
-  description            = "An attribute collected from a text field."
-  input_type             = "TEXT"
-  inventory_display_type = "HARDWARE"
-  data_type              = "STRING"
-}
+# # Text Field Example
+# resource "jamfpro_computer_extension_attribute" "computer_extension_attribute_text_field_1" {
+#   name                   = "tf-demo-text-field-example"
+#   enabled                = true
+#   description            = "An attribute collected from a text field."
+#   input_type             = "TEXT"
+#   inventory_display_type = "HARDWARE"
+#   data_type              = "STRING"
+# }
 
-# Script Example
-resource "jamfpro_computer_extension_attribute" "computer_extension_attribute_script_1" {
-  name                   = "tf-demo-hello-world"
-  enabled                = true
-  description            = "An attribute collected via a script."
-  input_type             = "SCRIPT"
-  script_contents        = "#!/bin/bash\necho 'Hello, World!!!!! :)'"
-  inventory_display_type = "GENERAL"
-  data_type              = "STRING"
-}
+# # Script Example
+# resource "jamfpro_computer_extension_attribute" "computer_extension_attribute_script_1" {
+#   name                   = "tf-demo-hello-world"
+#   enabled                = true
+#   description            = "An attribute collected via a script."
+#   input_type             = "SCRIPT"
+#   script_contents        = "#!/bin/bash\necho 'Hello, World!!!!! :)'"
+#   inventory_display_type = "GENERAL"
+#   data_type              = "STRING"
+# }
 
-# Script Example
-resource "jamfpro_computer_extension_attribute" "computer_extension_attribute_script_2" {
-  name                   = "tf-demo-logged-in-user"
-  enabled                = true
-  description            = "An attribute collected via a script."
-  input_type             = "SCRIPT"
-  script_contents        = file("support_files/computer_extension_attributes/logged_in_user.sh")
-  inventory_display_type = "GENERAL"
-  data_type              = "STRING"
-}
+# # Script Example
+# resource "jamfpro_computer_extension_attribute" "computer_extension_attribute_script_2" {
+#   name                   = "tf-demo-logged-in-user"
+#   enabled                = true
+#   description            = "An attribute collected via a script."
+#   input_type             = "SCRIPT"
+#   script_contents        = file("support_files/computer_extension_attributes/logged_in_user.sh")
+#   inventory_display_type = "GENERAL"
+#   data_type              = "STRING"
+# }
