1f9f4bc06788284b515f2ef9795fa3a3bb9496da
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-09-12 09:34:46 +0100

    Refactor app_installers.tf to remove unnecessary commented out code and plan status message

--- workload/terraform/jamfpro/scripts.tf
+++ workload/terraform/jamfpro/scripts.tf
@@ -1,37 +1,37 @@
-resource "jamfpro_script" "jamfpro_script_001" {
-  name            = "tf-demo-correct-application-permissions-v1.0"
-  script_contents = file("${path.module}/support_files/scripts/Correct Application Permissions.sh")
-  os_requirements = "13"
-  priority        = "BEFORE"
-  info            = "Adds target user or group to specified group membership, or removes said membership."
-  notes           = "Jamf Pro script parameters: 4 -> 7"
-  parameter4      = "Google Chrome" // targetApplication
-}
+# resource "jamfpro_script" "jamfpro_script_001" {
+#   name            = "tf-demo-correct-application-permissions-v1.0"
+#   script_contents = file("${path.module}/support_files/scripts/Correct Application Permissions.sh")
+#   os_requirements = "13"
+#   priority        = "BEFORE"
+#   info            = "Adds target user or group to specified group membership, or removes said membership."
+#   notes           = "Jamf Pro script parameters: 4 -> 7"
+#   parameter4      = "Google Chrome" // targetApplication
+# }
 
-resource "jamfpro_script" "jamfpro_script_002" {
-  name            = "tf-demo-encrypt-apfs-volume-v5.0.1"
-  script_contents = file("${path.module}/support_files/scripts/Encrypt_APFS_Volume.zsh")
-  os_requirements = "13"
-  priority        = "BEFORE"
-  info            = "Adds target user or group to specified group membership, or removes said membership."
-  notes           = "Jamf Pro script parameters: 4"
-  parameter4      = "/" // targetVolume
-}
+# resource "jamfpro_script" "jamfpro_script_002" {
+#   name            = "tf-demo-encrypt-apfs-volume-v5.0.1"
+#   script_contents = file("${path.module}/support_files/scripts/Encrypt_APFS_Volume.zsh")
+#   os_requirements = "13"
+#   priority        = "BEFORE"
+#   info            = "Adds target user or group to specified group membership, or removes said membership."
+#   notes           = "Jamf Pro script parameters: 4"
+#   parameter4      = "/" // targetVolume
+# }
 
-resource "jamfpro_script" "jamfpro_script_003" {
-  name            = "tf-demo-reset-safari-v2.1.4"
-  script_contents = file("${path.module}/support_files/scripts/Reset_Safari.sh")
-  os_requirements = "15"
-  priority        = "BEFORE"
-  info            = "Deleting Safari preference files to reset to system default."
-  notes           = "Jamf Pro script parameters: none"
-}
+# resource "jamfpro_script" "jamfpro_script_003" {
+#   name            = "tf-demo-reset-safari-v2.1.4"
+#   script_contents = file("${path.module}/support_files/scripts/Reset_Safari.sh")
+#   os_requirements = "15"
+#   priority        = "BEFORE"
+#   info            = "Deleting Safari preference files to reset to system default."
+#   notes           = "Jamf Pro script parameters: none"
+# }
 
-resource "jamfpro_script" "jamfpro_script_004" {
-  name            = "tf-demo-update-jamf-pro-inventory-and-set-logged-in-user-as-associated-user-v2.1.4"
-  script_contents = file("${path.module}/support_files/scripts/Update Jamf Pro Inventory and Set Logged-In User as Associated User.sh")
-  os_requirements = "15"
-  priority        = "BEFORE"
-  info            = "Update Jamf Pro inventory, assigning the computer record to the currently logged-in user.."
-  notes           = "Jamf Pro script parameters: none"
-}
+# resource "jamfpro_script" "jamfpro_script_004" {
+#   name            = "tf-demo-update-jamf-pro-inventory-and-set-logged-in-user-as-associated-user-v2.1.4"
+#   script_contents = file("${path.module}/support_files/scripts/Update Jamf Pro Inventory and Set Logged-In User as Associated User.sh")
+#   os_requirements = "15"
+#   priority        = "BEFORE"
+#   info            = "Update Jamf Pro inventory, assigning the computer record to the currently logged-in user.."
+#   notes           = "Jamf Pro script parameters: none"
+# }
