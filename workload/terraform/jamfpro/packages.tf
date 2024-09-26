8820d5cd359fdf9017154b9da3385f796b37c42f
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-09-26 16:08:25 +0100

    Refactor GitHub workflow files and remove unused secrets

--- workload/terraform/jamfpro/packages.tf
+++ workload/terraform/jamfpro/packages.tf
@@ -53,26 +53,26 @@ resource "jamfpro_package" "jamfpro_package_02" {
 }
 
 # // Example of referencing a package from a https source
-# resource "jamfpro_package" "jamfpro_package_03" {
-#   package_name          = "tf-demo-package-httpsourceprovider-companyportal"
-#   package_file_source   = "https://go.microsoft.com/fwlink/?linkid=853070"
-#   category_id           = "-1"
-#   info                  = "tf package deployment for demonstration"
-#   notes                 = "Uploaded by: terraform-provider-jamfpro plugin."
-#   priority              = 10
-#   reboot_required       = true
-#   fill_user_template    = false
-#   fill_existing_users   = false
-#   os_requirements       = "macOS 10.15.0"
-#   swu                   = false
-#   self_heal_notify      = false
-#   os_install            = false
-#   serial_number         = ""
-#   suppress_updates      = false
-#   ignore_conflicts      = false
-#   suppress_from_dock    = false
-#   suppress_eula         = false
-#   suppress_registration = false
-#   manifest              = ""
-#   manifest_file_name    = ""
-# }
+resource "jamfpro_package" "jamfpro_package_03" {
+  package_name          = "tf-demo-package-httpsourceprovider-companyportal"
+  package_file_source   = "https://go.microsoft.com/fwlink/?linkid=853070"
+  category_id           = "-1"
+  info                  = "tf package deployment for demonstration"
+  notes                 = "Uploaded by: terraform-provider-jamfpro plugin."
+  priority              = 10
+  reboot_required       = true
+  fill_user_template    = false
+  fill_existing_users   = false
+  os_requirements       = "macOS 10.15.0"
+  swu                   = false
+  self_heal_notify      = false
+  os_install            = false
+  serial_number         = ""
+  suppress_updates      = false
+  ignore_conflicts      = false
+  suppress_from_dock    = false
+  suppress_eula         = false
+  suppress_registration = false
+  manifest              = ""
+  manifest_file_name    = ""
+}
