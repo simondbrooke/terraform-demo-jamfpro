55a965f880dbeb1cf384464c1cb1aabc75dc1bf2
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-09-26 15:14:23 +0100

    Refactor computer_extension_attributes and printers in Terraform configuration

--- workload/terraform/jamfpro/printers.tf
+++ workload/terraform/jamfpro/printers.tf
@@ -1,20 +1,3 @@
-// Specific printer ppd
-resource "jamfpro_printer" "jamfpro_printers_001" {
-  name          = "tf-demo-printer-specific_ppd-01"
-  category_name = "No category assigned"
-  uri           = "lpd://10.1.20.204/"
-  cups_name     = "HP_DesignJet_1050C_PS3"
-  location      = "string"
-  model         = "HP DesignJet 1050C PS3"
-  info          = "string"
-  notes         = "string"
-  make_default  = true
-  use_generic   = false
-  ppd           = "HP_DesignJet_1050C_PS3.ppd"
-  ppd_path      = "/somepath"
-  ppd_contents  = file("/Users/dafyddwatkins/localtesting/support_files/printerppd/HP_DesignJet_1050C_PS3.ppd")
-}
-
 // Generic printer ppd
 resource "jamfpro_printer" "jamfpro_printers_002" {
   name          = "tf-demo-printer-generic_ppd-01"
