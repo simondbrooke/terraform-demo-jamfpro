682581feb03d36400966e05214066ebb5a9eae91
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-09-26 16:36:11 +0100

    Refactor GitHub workflow files and remove unused secrets

--- workload/terraform/jamfpro/packages.tf
+++ workload/terraform/jamfpro/packages.tf
@@ -1,27 +1,27 @@
 # // Example of referencing a package directly within the repository
-# resource "jamfpro_package" "jamfpro_package_001" {
-#   package_name          = "tf-demo-package-suspiciouspackage"
-#   package_file_source   = "support_files/packages/gha-test-SuspiciousPackage.dmg"
-#   category_id           = "-1" // jamfpro_category.jamfpro_category_001.id
-#   info                  = "tf package deployment for demonstration"
-#   notes                 = "Uploaded by: terraform-provider-jamfpro plugin."
-#   priority              = 10
-#   reboot_required       = true
-#   fill_user_template    = false
-#   fill_existing_users   = false
-#   os_requirements       = "macOS 10.15.0"
-#   swu                   = false
-#   self_heal_notify      = false
-#   os_install            = false
-#   serial_number         = ""
-#   suppress_updates      = false
-#   ignore_conflicts      = false
-#   suppress_from_dock    = false
-#   suppress_eula         = false
-#   suppress_registration = false
-#   manifest              = ""
-#   manifest_file_name    = ""
-# }
+resource "jamfpro_package" "jamfpro_package_001" {
+  package_name          = "tf-demo-package-suspiciouspackage"
+  package_file_source   = "support_files/packages/gha-test-SuspiciousPackage.dmg"
+  category_id           = "-1" // jamfpro_category.jamfpro_category_001.id
+  info                  = "tf package deployment for demonstration"
+  notes                 = "Uploaded by: terraform-provider-jamfpro plugin."
+  priority              = 10
+  reboot_required       = true
+  fill_user_template    = false
+  fill_existing_users   = false
+  os_requirements       = "macOS 10.15.0"
+  swu                   = false
+  self_heal_notify      = false
+  os_install            = false
+  serial_number         = ""
+  suppress_updates      = false
+  ignore_conflicts      = false
+  suppress_from_dock    = false
+  suppress_eula         = false
+  suppress_registration = false
+  manifest              = ""
+  manifest_file_name    = ""
+}
 
 
 # // https://go.microsoft.com/fwlink/?linkid=853070 - company portal
@@ -53,26 +53,26 @@ resource "jamfpro_package" "jamfpro_package_02" {
 }
 
 # // Example of referencing a package from a https source
-resource "jamfpro_package" "jamfpro_package_03" {
-  package_name          = "tf-demo-package-httpsourceprovider-companyportal"
-  package_file_source   = "https://go.microsoft.com/fwlink/?linkid=853070"
-  category_id           = "-1"
-  info                  = "tf package deployment for demonstration"
-  notes                 = "Uploaded by: terraform-provider-jamfpro plugin."
-  priority              = 10
-  reboot_required       = true
-  fill_user_template    = false
-  fill_existing_users   = false
-  os_requirements       = "macOS 10.15.0"
-  swu                   = false
-  self_heal_notify      = false
-  os_install            = false
-  serial_number         = ""
-  suppress_updates      = false
-  ignore_conflicts      = false
-  suppress_from_dock    = false
-  suppress_eula         = false
-  suppress_registration = false
-  manifest              = ""
-  manifest_file_name    = ""
-}
+# resource "jamfpro_package" "jamfpro_package_03" {
+#   package_name          = "tf-demo-package-httpsourceprovider-companyportal"
+#   package_file_source   = "https://go.microsoft.com/fwlink/?linkid=853070"
+#   category_id           = "-1"
+#   info                  = "tf package deployment for demonstration"
+#   notes                 = "Uploaded by: terraform-provider-jamfpro plugin."
+#   priority              = 10
+#   reboot_required       = true
+#   fill_user_template    = false
+#   fill_existing_users   = false
+#   os_requirements       = "macOS 10.15.0"
+#   swu                   = false
+#   self_heal_notify      = false
+#   os_install            = false
+#   serial_number         = ""
+#   suppress_updates      = false
+#   ignore_conflicts      = false
+#   suppress_from_dock    = false
+#   suppress_eula         = false
+#   suppress_registration = false
+#   manifest              = ""
+#   manifest_file_name    = ""
+# }
